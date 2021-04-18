694215ef127c16af8fc3e638a650ecc2e8a7e456
evatro.uygun@daimler.com <evatro.uygun@daimler.com>
2021-04-18 13:14:02 +0300

    initial

---
+++ src/main/java/com/cdc/tr/cdcdebezium/CdcDebeziumApplication.java
@@ -0,0 +1,15 @@
+package com.cdc.tr.cdcdebezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CdcDebeziumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcDebeziumApplication.class, args);
+	}
+
+}
